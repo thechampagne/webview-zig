9176427e7ba237b7f492ca8056e31de0e6cf4fbb
XXIV <13811862+thechampagne@users.noreply.github.com>
2024-06-12 21:50:53 +0300

    build.zig: fix missing glib.h on freebsd

--- build.zig
+++ build.zig
@@ -63,6 +63,7 @@ pub fn build(b: *std.Build) void {
         .freebsd => {
             staticLib.addCSourceFile(.{ .file = .{ .path = "external/webview/webview.cc"}, .flags = &.{"-std=c++11"}});
             staticLib.addIncludePath(.{ .path = "/usr/local/include/gtk-3.0/"});
+            staticLib.addIncludePath(.{ .path = "/usr/local/include/glib-2.0/"});
             staticLib.addIncludePath(.{ .path = "/usr/local/include/webkitgtk-4.0/"});
             staticLib.linkSystemLibrary("gtk-3");
             staticLib.linkSystemLibrary("webkit2gtk-4.0");
@@ -100,6 +101,7 @@ pub fn build(b: *std.Build) void {
         .freebsd => {
             sharedLib.addCSourceFile(.{ .file = .{ .path = "external/webview/webview.cc"}, .flags = &.{"-std=c++11"}});
             sharedLib.addIncludePath(.{ .path = "/usr/local/include/gtk-3.0/"});
+            sharedLib.addIncludePath(.{ .path = "/usr/local/include/glib-2.0/"});
             sharedLib.addIncludePath(.{ .path = "/usr/local/include/webkitgtk-4.0/"});
             sharedLib.linkSystemLibrary("gtk-3");
             sharedLib.linkSystemLibrary("webkit2gtk-4.0");
