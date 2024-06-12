3efc0714dc34338e946886bdd638aa53f64fdb81
XXIV <13811862+thechampagne@users.noreply.github.com>
2024-06-13 01:39:04 +0300

    build.zig: fix missing atk.h on freebsd

--- build.zig
+++ build.zig
@@ -70,6 +70,7 @@ pub fn build(b: *std.Build) void {
             staticLib.addIncludePath(.{ .path = "/usr/local/include/pango-1.0/"});
             staticLib.addIncludePath(.{ .path = "/usr/local/include/harfbuzz/"});
             staticLib.addIncludePath(.{ .path = "/usr/local/include/gdk-pixbuf-2.0/"});
+            staticLib.addIncludePath(.{ .path = "/usr/local/include/atk-1.0/"});
             staticLib.linkSystemLibrary("gtk-3");
             staticLib.linkSystemLibrary("webkit2gtk-4.0");
         },
@@ -113,6 +114,7 @@ pub fn build(b: *std.Build) void {
             sharedLib.addIncludePath(.{ .path = "/usr/local/include/pango-1.0/"});
             sharedLib.addIncludePath(.{ .path = "/usr/local/include/harfbuzz/"});
             sharedLib.addIncludePath(.{ .path = "/usr/local/include/gdk-pixbuf-2.0/"});
+            sharedLib.addIncludePath(.{ .path = "/usr/local/include/atk-1.0/"});
             sharedLib.linkSystemLibrary("gtk-3");
             sharedLib.linkSystemLibrary("webkit2gtk-4.0");
         },
